6a3d204bb1e6c3f1cfebe59734abb33cdea9d571
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-11-08 22:52:14 +0100

    server-side register form validation done

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,16 +4,17 @@ from django.urls import reverse
 from accounts.models import User
 # Create your tests here.
 
-class RegisterPageTextCase(TestCase):
+class RegisterPageTestCase(TestCase):
 
     def test_register_page(self):
         response = self.client.get(reverse('register'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(reverse('register'))
 
     def test_new_user_registration_nominal_case(self):
         registered_user_old = User.objects.count()
 
-        email="test@test.fr"
+        email="test2@test.fr"
         password = "LePetitChevalDeManège123123!"
         last_name = "test last"
         first_name = "test first"
@@ -21,12 +22,12 @@ class RegisterPageTextCase(TestCase):
         response = self.client.post(reverse('register'),
                                                 {'email':email,
                                                 'password':password,
+                                                'password2':password,
                                                 'last_name':last_name,
                                                 'first_name':first_name})
-        
         registered_user_new = User.objects.count()
-        
         self.assertEqual(registered_user_old + 1, registered_user_new)
+        self.assertEqual(response.status_code,302)
         
     def test_new_user_registration_case_void_facultative_fields(self):
         registered_user_old = User.objects.count()
@@ -36,8 +37,74 @@ class RegisterPageTextCase(TestCase):
 
         response = self.client.post(reverse('register'),
                                                 {'email':email,
-                                                'password':password,})
+                                                'password':password,
+                                                'password2':password})
         
         registered_user_new = User.objects.count()
-        
         self.assertEqual(registered_user_old + 1, registered_user_new)
+        self.assertEqual(response.status_code,302)
+
+    def test_user_registration_already_taken_email(self):
+        email="test@test.fr"
+        password = "LePetitChevalDeManège123123!"
+        last_name = "test last"
+        first_name = "test first"
+        self.client.post(reverse('register'),
+                                    {'email':email,
+                                    'password':password,
+                                    'password2':password,
+                                    'last_name':last_name,
+                                    'first_name':first_name})
+        
+        response = self.client.post(reverse('register'),
+                                                {'email':email,
+                                                'password':password,
+                                                'password2':password,
+                                                'last_name':last_name,
+                                                'first_name':first_name})
+
+        self.assertEquals(response.status_code,400)
+
+    def test_user_registration_invalid_email(self):
+        email="test_email_invalide"
+        password = "LePetitChevalDeManège123123!"
+        last_name = "test last"
+        first_name = "test first"        
+        response = self.client.post(reverse('register'),
+                                                {'email':email,
+                                                'password':password,
+                                                'password2':password,
+                                                'last_name':last_name,
+                                                'first_name':first_name})
+
+        self.assertEquals(response.status_code,400)
+
+    
+    def test_user_registration_short_password(self):
+        email="test-password@trop.court"
+        password = "pass"
+        last_name = "test last"
+        first_name = "test first"        
+        response = self.client.post(reverse('register'),
+                                                {'email':email,
+                                                'password':password,
+                                                'password2':password,
+                                                'last_name':last_name,
+                                                'first_name':first_name})
+
+    def test_user_registration_unmatching_passwords(self):
+        email="test-password@diff.erents"
+        password = "LePetitChevalDeManège123123!"
+        password2 = "LePetitChevalDeManège123123!0"
+        last_name = "test last"
+        first_name = "test first"        
+        response = self.client.post(reverse('register'),
+                                                {'email':email,
+                                                'password':password,
+                                                'password2':password2,
+                                                'last_name':last_name,
+                                                'first_name':first_name})
+
+
+class LoginTest(TestCase):
+    pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from validate_email import validate_email
+
 from .models import User
 
 # Create your views here.
@@ -8,22 +10,48 @@ from .models import User
 # create
 def register(request):
     if request.method=='POST':
+        has_error = False
+        # get form elements
         email = request.POST.get('email')
         password = request.POST.get('password')
+        password2 = request.POST.get('password2')
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
-        user = User(email=email, password=password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-        if user is not None:
-            login(request, user)
-            messages.error(request, f"{user.email} logged in")
-            return render(request, 'products/index.html')
+        
+        # check integrity of data
+        if not password==password2:
+            messages.error(request, f"mots de passe différents")
+            has_error=True
+        
+        if len(password) <= 6:
+            messages.error(request, f"mot de passe trop court")
+            has_error=True
+        
+        if not validate_email(email):
+            messages.error(request, f"email invalide")
+            has_error=True
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, f"email déjà utilisé")
+            has_error=True
+
+        if has_error:
+            return render(request, 'accounts/register.html',status=400)
         else:
-            messages.error(request, "echec de l'enregistrement")
-            return render(request, 'accounts/register.html')
+            # save the user in database
+            user = User(email=email, password=password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+
+            # then connect this user
+            if user is not None:
+                login(request, user)
+                return redirect('products/index')
+            else:
+                return render(request, 'accounts/register.html')      
     else:
+        # if it is a get method, render the User registration form
         return render(request, 'accounts/register.html')
 
 # read
@@ -34,9 +62,6 @@ def profile(request):
 
     return render(request, 'accounts/profile.html')
 
-# update
-
-# delete
 
 # authentification views 
 def login_user(request):
