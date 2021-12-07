d39b5ed565ee4a35a7b5bcd178ec43136417a2cd
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-12-07 13:41:03 +0100

    :bugfix: missing whitenoise in middleware

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -91,6 +91,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+    MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 else:
     # SECURITY WARNING: keep the secret key used in production secret!
@@ -99,7 +100,7 @@ else:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
     ALLOWED_HOSTS = []
-    MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+    
 
 
 # Password validation
