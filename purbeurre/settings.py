23714dfb0d6514a09ea5f572451e8b4dd764581f
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-12-07 12:13:27 +0100

    🐛 staticfiles options in settings.py

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -155,3 +155,11 @@ AUTH_USER_MODEL = 'accounts.User'
 
 #pour @login_required
 LOGIN_URL = '/user/login'
+
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
+    STATICFILES_DIRS = [
+        os.path.join(PROJECT_ROOT, 'static')
+    ]
