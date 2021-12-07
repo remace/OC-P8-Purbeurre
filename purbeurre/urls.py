a5c5382ef660b579591b4b43999c3fdad00f4258
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-12-07 12:40:30 +0100

    🐛 removed django debug toolbar route

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-import debug_toolbar
-
+import os
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,5 +22,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('user/', include('accounts.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+if os.environ.get('ENV')!= 'PRODUCTION':
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
+    
