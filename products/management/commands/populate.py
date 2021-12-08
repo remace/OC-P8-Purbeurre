37ea829a86f9b8057ee40173391ff4ba8840e557
Rémi TAUVEL <tauvel.remi@gmail.com>
2021-12-08 15:28:56 +0100

    ⚡️ populate command change to get all products ✨ better template on products page

--- products/management/commands/populate.py
+++ products/management/commands/populate.py
@@ -19,6 +19,43 @@ def get_products_from_category(url, n_products):
             rep.append(product)
     return rep
 
+def create_product_or_None(prod : dict, category):
+    """ given a dict, this function returns a product object if possible or none """
+    try:
+        if 'energy-kcal_100g' in prod['nutriments']:
+            energy_unit = 'kcal'
+            energy_100g = prod['nutriments'].get('energy-kcal_100g',-1)
+        else:
+            energy_unit = 'kJ'
+            energy_100g = prod['nutriments'].get('energy-kj_100g',-1)
+
+        product = Product(
+            name = prod['product_name'],
+            nutriscore = prod.get('nutriscore_grade','F').upper(),
+            energy_unit = energy_unit,
+            energy_100g = round(energy_100g,9),
+            carbohydrates_100g = round(prod['nutriments'].get('carbohydrates_100g',-1),3),
+            sugars_100g = round(prod['nutriments'].get('sugars_100g',-1),3),
+            fat_100g = round(prod['nutriments'].get('fat_100g',-1),3),
+            saturated_fat_100g = round(prod['nutriments'].get('saturated_fat_100g',-1),3),
+            fiber_100g = round(prod['nutriments'].get('fiber_100g',-1),3),
+            proteins_100g = round(prod['nutriments'].get('proteins_100g',-1),3),
+            salt_100g = round(prod['nutriments'].get('salt_100g',-1),3),
+            sodium_100g = round(prod['nutriments'].get('sodium_100g',-1),3),
+            off_link = f'https://fr.openfoodfacts.org/produit/{ prod["id"]}',
+            off_thumb_link = prod['image_front_thumb_url'],
+            off_img_link = prod['image_front_url'],
+            category = category,
+        )
+        return product
+    except KeyError as error:
+
+        print(f'KeyError: {prod["product_name"]}\tclé:{error}'\
+        f'\thttps://fr.openfoodfacts.org/api/v0/produit/{prod["id"]}.json'\
+        f'\n=> produit ignoré'
+        )
+        return None
+
 class Command(BaseCommand):
     ''' populates the database with x products from y categories'''
 
@@ -55,41 +92,7 @@ class Command(BaseCommand):
                     cats_json['tags'][cat]['url'],
                     n_products)[0:n_products]
             for prod in products:
-                try:
-                    if 'energy-kcal_100g' in prod['nutriments']:
-                        energy_unit = 'kcal'
-                        energy_100g = prod['nutriments']['energy-kcal_100g']
-                    else:
-                        energy_unit = 'kJ'
-                        energy_100g = prod['nutriments']['energy-kj_100g']
-
-                    if 'nutriscore_grade' in prod:
-                        product = Product(
-                            name = prod['product_name'],
-                            nutriscore = prod['nutriscore_grade'].upper(),
-                            energy_unit = energy_unit,
-                            energy_100g = round(energy_100g,9),
-                            carbohydrates_100g = round(prod['nutriments']['carbohydrates_100g'],3),
-                            sugars_100g = round(prod['nutriments']['sugars_100g'],3),
-                            fat_100g = round(prod['nutriments']['fat_100g'],3),
-                            saturated_fat_100g = round(prod['nutriments']['saturated-fat_100g'],3),
-                            fiber_100g = round(prod['nutriments'].get('fiber_100g',-1),3),
-                            proteins_100g = round(prod['nutriments']['proteins_100g'],3),
-                            salt_100g = round(prod['nutriments']['salt_100g'],3),
-                            sodium_100g = round(prod['nutriments']['sodium_100g'],3),
-                            off_link = f'https://fr.openfoodfacts.org/produit/{ prod["id"]}',
-                            off_thumb_link = prod['image_front_thumb_url'],
-                            off_img_link = prod['image_front_url'],
-                            category = category,
-                        )
-                except KeyError as error:
-
-                    print(f'ERROR: {prod["product_name"]}\tclé:{error}'\
-                    f'\thttps://fr.openfoodfacts.org/api/v0/produit/{prod["id"]}.json'
-                    )
-
-                    # print(prod['nutriments'])
-                else:
-                    # print(f'\t{count+1}/{n_products}\t{prod["product_name"]}')
+                product = create_product_or_None(prod,category)
+                if product is not None:
                     product.save()
                     
