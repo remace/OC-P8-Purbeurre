7314f6c6279fa81e160c52fa056bc7524c98e709
Rémi TAUVEL <tauvel.remi@gmail.com>
2022-03-08 07:37:37 +0100

    scrutation de la date

--- products/management/commands/update.py
+++ products/management/commands/update.py
@@ -1,8 +1,7 @@
 import requests
 import gzip
 import json
-
-
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 from products.models import Product, Category
@@ -29,19 +28,33 @@ class Command(BaseCommand):
         files_list = files_text.split('\n')
         files_to_download=files_list[0:7]
 
-        # print(files_to_download)
-        update_data = []
-        for filename in files_to_download:
-            req = requests.get(f'https://static.openfoodfacts.org/data/delta/{filename}', allow_redirects=True)
-            with open(f'modified/{filename}', 'wb') as gz_file:
-                gz_file.write(req.content)
-    
-            with gzip.open(f'modified/{filename}', 'rb') as gz_file:
-                update_data.append(gz_file.read().decode())
+        # for filename in files_to_download:
 
-        with open(f'modified/changelist/{filename}', 'w') as json_file:
-            json_file.write(str(update_data))
+        #     # download update data archive
+        #     req = requests.get(f'https://static.openfoodfacts.org/data/delta/{filename}', allow_redirects=True)
+        #     with open(f'modified/{filename}', 'wb') as gz_file:
+        #         gz_file.write(req.content)
+        
 
 
+        with open(f'modified/changelist/changelist.json', 'a') as json_file:
+            for filename in files_to_download:
+                # decode update data archive
+                with gzip.open(f'modified/{filename}', 'rb') as gz_file:
+                    update_data_list_strings = [data for data in gz_file.read().decode().split('\n') if data != ""]
+                    update_data_list_dicts = []
+                    for data in update_data_list_strings:
+                        update_data_list_dicts.append(json.loads(data))
+            json_file.write(str(update_data_list_dicts))
 
+        count=0
+        updated_count=0
+        for product_dict in update_data_list_dicts:
+            products = Product.objects.filter(off_link=f' https://world.openfoodfacts.org/api/v0/product/{product_dict.get("id")}.json').filter(updated_at__gte=datetime.utcfromtimestamp(int(product_dict.get("last_modified_t"))/1000.0)).all()
+            count+=1
+            if len(products)>=1:
+                for product in products:
+                    print(product.name)
+                updated_count+=len(products)
 
+        print(f"{count} produits mis à jour chez open food facts\n {updated_count} mis à jour ici")
